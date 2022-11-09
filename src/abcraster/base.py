5f7cc7d3be9185cc3acdcfe8688a9dc2dba59fa0
marxt <marxtupas@gmail.com>
2022-11-09 21:30:13 +0100

    rasterized path bugfix for multiple inputs

--- src/abcraster/base.py
+++ src/abcraster/base.py
@@ -318,7 +318,8 @@ def run(ras_data_filepaths, ref_data_filepath, out_dirpath, metrics_list, sample
 
         ref_file_ext = os.path.splitext(os.path.basename(ref_data_filepath))[1]
         if ref_file_ext == '.shp':
-            ref_data_filepath_current = v_rasterized_filename  # use the temporary rasterized reference
+            v_rasterized_path = os.path.join(out_dirpath, v_rasterized_filename)
+            ref_data_filepath_current = v_rasterized_path  # use the temporary rasterized reference
 
         if i == num_inputs - 1: #  if last run use delete flag from cli
             if delete_tmp_files:
